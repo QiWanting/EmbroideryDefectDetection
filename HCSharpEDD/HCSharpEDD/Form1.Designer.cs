5ddba4d5ad46a4bae8f154641b229f378ed29ddb
AP\qiwa <wanting.qi@brother-bsh.com.cn>
2024-09-13 18:13:09 +0800

    Demo初步完成，识别猫的功能

--- HCSharpEDD/HCSharpEDD/Form1.Designer.cs
+++ HCSharpEDD/HCSharpEDD/Form1.Designer.cs
@@ -3,14 +3,14 @@
     partial class Form1
     {
         /// <summary>
-        /// 必需的设计器变量。
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// 清理所有正在使用的资源。
+        ///  Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,28 +20,77 @@
             base.Dispose(disposing);
         }
 
-        #region Windows 窗体设计器生成的代码
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// 设计器支持所需的方法 - 不要修改
-        /// 使用代码编辑器修改此方法的内容。
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
+            pictureBox_display = new PictureBox();
+            button_identify = new Button();
+            label_time = new Label();
+            label_time_ = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox_display).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox_display
+            // 
+            pictureBox_display.Location = new Point(12, 12);
+            pictureBox_display.Name = "pictureBox_display";
+            pictureBox_display.Size = new Size(1298, 1031);
+            pictureBox_display.TabIndex = 0;
+            pictureBox_display.TabStop = false;
+            // 
+            // button_identify
+            // 
+            button_identify.Location = new Point(1333, 12);
+            button_identify.Name = "button_identify";
+            button_identify.Size = new Size(270, 32);
+            button_identify.TabIndex = 1;
+            button_identify.Text = "开始识别";
+            button_identify.UseVisualStyleBackColor = true;
+            button_identify.Click += button_identify_Click;
+            // 
+            // label_time
+            // 
+            label_time.AutoSize = true;
+            label_time.Location = new Point(1333, 60);
+            label_time.Name = "label_time";
+            label_time.Size = new Size(68, 17);
+            label_time.TabIndex = 2;
+            label_time.Text = "运行时间：";
+            // 
+            // label_time_
+            // 
+            label_time_.AutoSize = true;
+            label_time_.Location = new Point(86, 1029);
+            label_time_.Name = "label_time_";
+            label_time_.Size = new Size(0, 17);
+            label_time_.TabIndex = 2;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "HCSharpEDD";
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1615, 1055);
+            Controls.Add(label_time_);
+            Controls.Add(label_time);
+            Controls.Add(button_identify);
+            Controls.Add(pictureBox_display);
+            Name = "Form1";
+            Text = "HCSharpEDD";
+            ((System.ComponentModel.ISupportInitialize)pictureBox_display).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox_display;
+        private Button button_identify;
+        private Label label_time;
+        private Label label_time_;
     }
 }
-
--- HCSharpEDD/HCSharpEDD/Form1.cs
+++ HCSharpEDD/HCSharpEDD/Form1.cs
@@ -1,12 +1,8 @@
-锘縰sing System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Drawing.Imaging;
+using Yolov5Net.Scorer;
+using Yolov5Net.Scorer.Models;
 
 namespace HCSharpEDD
 {
@@ -15,6 +11,56 @@ namespace HCSharpEDD
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string pictureFilePath =
+            "D:\\own\\2024\\tian\\yolov8quexianjiance\\Code\\EmbroideryDefectDetection\\HCSharpEDD\\Data\\cat.14.jpg";
+        private string onnxFilePath =
+            "D:\\own\\2024\\tian\\yolov8quexianjiance\\Code\\EmbroideryDefectDetection\\HCSharpEDD\\Data\\Weights\\best.onnx";
+
+        private void button_identify_Click(object sender, EventArgs e)
+        {
+            var image = System.Drawing.Image.FromFile(pictureFilePath);
+            Image<Rgba32> outputImage;
+            #region 将bitmap类型转换成Image<Rgba32>
+            using (var inputImage = System.Drawing.Image.FromFile(pictureFilePath))
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    inputImage.Save(memoryStream, ImageFormat.Png);
+                    memoryStream.Position = 0;
+                    outputImage = SixLabors.ImageSharp.Image.Load<Rgba32>(memoryStream);
+                }
+            }
+            #endregion
+
+            #region 识别并框出物体
+            DateTime T_start = DateTime.Now;
+            var scorer = new YoloScorer<YoloCocoP5Model>(onnxFilePath);
+            List<YoloPrediction> predictions = scorer.Predict(outputImage);
+            var graphics = Graphics.FromImage(image);
+            foreach (var prediction in predictions)
+            {
+                double score = Math.Round(prediction.Score, 2);
+
+                SixLabors.ImageSharp.RectangleF rectangleRange = prediction.Rectangle;
+                System.Drawing.RectangleF rectangle = new System.Drawing.RectangleF(
+                    rectangleRange.X, rectangleRange.Y, rectangleRange.Width, rectangleRange.Height);
+                graphics.DrawRectangles(new System.Drawing.Pen(System.Drawing.Color.Red, 1),
+                new[] { rectangle });
+                var (x, y) = (prediction.Rectangle.X - 3, prediction.Rectangle.Y - 23);
+
+                graphics.DrawString($"{prediction.Label.Name} ({score})",
+                    new Font("Consolas", 16, GraphicsUnit.Pixel), new SolidBrush(System.Drawing.Color.Red),
+                    new System.Drawing.PointF(x, y));
+            }
+            image.Save("result.jpg");
+            this.pictureBox_display.Image = image;
+            DateTime T_end = DateTime.Now;
+            #endregion
+
+            TimeSpan T_esp = T_end - T_start;
+            this.label_time_.Text = T_esp.TotalMilliseconds.ToString();
         }
     }
 }
